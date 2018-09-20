058433de8321978bf7ff303c8297234ccf9cfb26
Qjchen <cqj8660@163.com>
2018-09-20 23:27:17 +0800

    before D

---
+++ #401_div2/D.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define pii pair<int, int>
+using namespace std;
+const int maxn = 5e5 + 10;
+string s[maxn];
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int n;
+    cin >> n;
+    for(int i = 0; i < n; i++)
+        cin >> s[i];
+    
+    return 0;
+}

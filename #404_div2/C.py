3405120837f9f4f4b9a8f6e9052ec93e8ee31143
Qjchen <cqj8660@163.com>
2018-10-02 22:27:32 +0800

    404C python

---
+++ #404_div2/C.py
@@ -0,0 +1,14 @@
+n, m = map(int, input().split())
+if m>=n:
+    print(n)
+else:
+    l = 1
+    r = 2000000000
+    for i in range(100):
+        mid = int((l + r) / 2)
+        if((mid) * (mid + 1) >= 2 * (n - m)):
+            r = mid
+        else:
+            l = mid + 1
+    print(m + r)
+

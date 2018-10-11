d763ba7bf4c78637793e05a5b3b8e1dcf297f480
Qjchen <cqj8660@163.com>
2018-10-11 20:44:04 +0800

    405 D

--- #405_div2/D.cpp
+++ #405_div2/D.cpp
@@ -8,17 +8,32 @@ int layer[maxn];
 int cnt, n, k;
 ll ans;
 ll sum[maxn];
+int subtract(int a, int b)
+{
+    return ((a - b) % k + k) % k;
+}
 void dfs(int node, int fa, int lay)
 {
-    layer[node] = lay;
+    sum[node] = dp[node][lay % k] = 1;
     for(auto idx: tree[node])
     {
         if(idx == fa) continue;
         dfs(idx, node, lay + 1);
-        
+        for(int i = 0; i < k; i++)
+            for(int j = 0; j < k; j++)
+            {
+                int r = subtract(i + j, 2 * lay);
+                // compute distance modulo k
+                int t = subtract(k, r);
+                // compute x such that (distance + x) is divisible by k
+                ans += t * dp[node][i] * dp[idx][j];
+            }
+        for(int i = 0; i < k; i++)
+            dp[node][i] += dp[idx][i];
         sum[node] += sum[idx];
     }
-    
+    // in how many pairs we will count the edge from 'a' to its parent?
+    ans += sum[node] * (n - sum[node]);
 }
 int main()
 {
@@ -33,8 +48,6 @@ int main()
         tree[b].push_back(a);
     }
     dfs(1, -1, 0);
-//    for(int i = 1; i <= n; i++)
-//        cout << dp[i] << endl;
     cout << ans / k << endl;
     return 0;
 }

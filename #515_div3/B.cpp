57a3357259dc0cfecd616f3a024b9f9db258f93a
Qjchen <cqj8660@163.com>
2018-10-13 13:30:50 +0800

    515 C

--- #515_div3/B.cpp
+++ #515_div3/B.cpp
@@ -2,14 +2,8 @@
 using namespace std;
 #define ll long long
 int n, r, t;
-int solve(int n)
-{
-    int len = (2 * r - 1);
-    int temp = n / len;
-    if(n % len) temp++;
-    return temp;
-}
-int a[2000];
+const int maxn = 2e3 + 10;
+int a[maxn], b[maxn], dp[maxn];
 int main()
 {
     ios::sync_with_stdio(false);
@@ -19,10 +13,35 @@ int main()
     for(int i = 0; i < n; i++)
     {
         cin >> a[i];
+        if(a[i])cnt++;
     }
     for(int i = 0; i < n; i++)
     {
-        
+        if(a[i])
+            for(int j = max(0, i - r + 1); j < min(n, i + r); j++)
+                dp[j]++;
     }
+    for(int i = 0; i < n; i++)
+        if(dp[i] == 0)
+        {
+            cout << -1;
+            return 0;
+        }
+    for(int i = 0; i < n; i++)
+    {
+        int f = 1;
+        for(int j = max(0, i - r + 1); j < min(n, i + r); j++)
+        {
+            dp[j]--;
+            if(dp[j] == 0) f = 0;
+        }
+        if(f) cnt--;
+        else
+        {
+            for(int j = max(0, i - r + 1); j < min(n, i + r); j++)
+                dp[j]++;
+        }
+    }
+    cout << cnt << endl;
     return 0;
 }

46e8bcde8a552010b1c221a68a48d74294f3237c
Qjchen <cqj8660@163.com>
2018-10-14 02:10:29 +0800

    407 div2

--- #406_div2/B.cpp
+++ #406_div2/B.cpp
@@ -1,59 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
-map<ll, int> g;
-set<ll> ans;
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(0);
-    ll b, q, l, m;
-    cin >> b >> q >> l >> m;
-    if(abs(b) > l)
+    int n, m;
+    cin >> n >> m;
+    while(m--)
     {
-        cout << 0 << endl;
-        return 0;
-    }
-    for(int i = 0; i < m; i++)
-    {
-        int m; cin >> m;
-        g[m] = 1;
-    }
-    if(q == 0 || b == 0)
-    {
-        if(g[0]){
-            if(g[b])
-                cout << 0 << endl;
-            else
-                cout << 1 << endl;
+        int k; cin >> k;
+        map<int, int> q;
+        bool f = 1;
+        while(k--)
+        {
+            int t; cin >> t;
+            q[t] = 1;
+            if(q[-t]) f = 0;
         }
-        else
-            cout << "inf" << endl;
-        return 0;
+        if(f)
+        {
+            cout << "YES" << endl;
+            return 0;
+        }
     }
-    if(q == 1)
-    {
-        if(g[b] == 0)
-            cout << "inf" << endl;
-        else
-            cout << 0 << endl;
-        return 0;
-    }
-    if(q == -1)
-    {
-        if((g[b] == 0 ||  g[-b] == 0))
-            cout << "inf" << endl;
-        else
-            cout << 0 << endl;
-        return 0;
-    }
-    int size = !g[b];//判断第一位是不是存在于序列中
-    while(abs(b * q) <= l)
-    {
-        b *= q;
-        if(g[b] == 0)
-            size++;
-    }
-    cout << size << endl;
+    cout << "NO" << endl;
     return 0;
 }

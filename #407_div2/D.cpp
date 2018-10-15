08cb728fed3d99b8f977f7972813edc0c54e556b
Qjchen <cqj8660@163.com>
2018-10-15 20:07:02 +0800

    407 D 待补

---
+++ #407_div2/D.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define ll long long
+const int maxn = 1e5 + 10;
+using namespace std;
+int n, m;
+vector<int> g[maxn];
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cin >> n >> m;
+    for(int i = 0; i < m; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+        g[u].push_back(v);
+        g[v].push_back(u);
+    }
+    if(m < n - 1)
+    {
+        cout << 0 << endl;
+        return 0;
+    }
+    return 0;
+}

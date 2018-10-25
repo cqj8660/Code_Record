49f01c3731f5cf3c8a992ae14f120e57abbdc5a3
Qjchen <cqj8660@163.com>
2018-10-26 02:06:49 +0800

    EDU53 C

--- EDU#53/C.cpp
+++ EDU#53/C.cpp
@@ -2,56 +2,67 @@
 #define fi first
 #define se second
 #define ll long long
-const int maxn = 1e5 + 10;
+const int maxn = 2e5 + 10;
 using namespace std;
 pair<int, int> q[maxn];
+int n, x, y;
+char s[maxn];
+bool check(int mid)
+{
+    for(int i = 1; i <= n - mid + 1; i++)
+    {
+        int l = i, r = i + mid - 1;
+        int nedx = x - q[i - 1].fi - (q[n].fi - q[r].fi);
+        int nedy = y - q[i - 1].se - (q[n].se - q[r].se);
+        if(mid >= abs(nedx) + abs(nedy) && ((abs(nedx) + abs(nedy)) % 2 == mid % 2))
+            return true;
+    }
+    return false;
+}
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(0);
-    int n;
-    string s;
-    cin >> n >> s;
-    int x, y;
+    cin >> n >> (s + 1);
     cin >> x >> y;
     q[0] = make_pair(0, 0);
-    if(s[0] == 'R')
-        q[0].se++;
-    else if(s[0] == 'L')
-        q[0].se--;
-    else if(s[0] == 'U')
-        q[0].fi++;
-    else
-        q[0].fi--;
-    for(int i = 0; i < n; i++)
+    for(int i = 1; i <= n; i++)
     {
         switch(s[i])
         {
         case 'R':
-            q[i] = make_pair(q[i - 1].fi, q[i - 1].se + 1);
-            break;
-        case 'L':
-            q[i] = make_pair(q[i - 1].fi, q[i - 1].se - 1);
-            break;
-        case 'U':
             q[i] = make_pair(q[i - 1].fi + 1, q[i - 1].se);
             break;
-        case 'D':
+        case 'L':
             q[i] = make_pair(q[i - 1].fi - 1, q[i - 1].se);
+            break;
+        case 'U':
+            q[i] = make_pair(q[i - 1].fi, q[i - 1].se + 1);
+            break;
+        case 'D':
+            q[i] = make_pair(q[i - 1].fi, q[i - 1].se - 1);
             break;
         }
     }
-    if(q[n - 1].fi == x && q[n - 1].se == y)
+    if(q[n].fi == x && q[n].se == y)
     {
         cout << 0 << endl;
         return 0;
     }
-    int l = 0, r = 0, ans = -1;
-    while(i <= j && j < n)
+    int l = 0, r = n;
+    int ans = -1;
+    for(int i = 0; i < 100; i++)
     {
-
+        int mid = (l + r) >> 1;
+        if(check(mid))
+        {
+            r = mid;
+            ans = r;
+        }
+        else
+            l = mid;
     }
-    cout << endl;
+    cout << ans << endl;
     return 0;
 
 }

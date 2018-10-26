4cd138b683ba8decb4fc150b0247fdb2fcd6a7d4
Qjchen <cqj8660@163.com>
2018-10-26 23:57:07 +0800

    ..

---
+++ EOJ_Oct/C.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define pii pair<int, int>
+#define ll long long
+#define eps 1e-6
+using namespace std;
+const int maxn = 2e5 + 10;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int n, k, q;
+    cin >> n >> k >> q;
+}

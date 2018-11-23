9cb36bf5c06801928c782383bbac21ac90970a40
Qjchen <cqj8660@163.com>
2018-11-23 11:43:06 +0800

    ..

--- #523_div2/C.cpp
+++ #523_div2/C.cpp
@@ -1,36 +1,36 @@
 #include <bits/stdc++.h>
 #define ll long long
-const int maxn = 1e5 + 10;
+const int maxn = 1e6 + 10;
+const int MOD = 1e9 + 7;
 using namespace std;
-int a[maxn];
-int num[maxn];
-int n;
-void solve(int v)
-{
-    int u = sqrt(v) + 1;
-    for(int i = 1; i <= u; i++)
-    {
-        if(i > n) break;
-        if(v % i)
-        {
-            num[i]++;
-            if(i != v / i && v / i <= n)
-                num[v / i]++;
-        }
-    }
-}
+ll a[maxn], dp[maxn], ans, upper;
+vector<int> factor[maxn];
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(0);
+    int n;
     cin >> n;
-    for(int i = 0; i < n; i++)
-        cin >> a[i];
-    for(int i = 0; i < n; i++)
-        solve(a[i]);
     for(int i = 1; i <= n; i++)
     {
-        
+        cin >> a[i];
+        upper = max(upper, a[i]);
     }
+    for(int i = 1; i <= n; i++)
+        for(int j = i; j <= upper; j += i)
+            factor[j].push_back(i);
+    dp[0] = 1;
+    for(int i = 1; i <= n; i++)
+    {
+        int len = (int)factor[a[i]].size();
+        ll t = a[i];
+        for(int j = len - 1; j >= 0; j--)
+        {
+            int x = factor[t][j];
+            ans = (ans + dp[x - 1]) % MOD;
+            dp[x] = (dp[x] + dp[x - 1]) % MOD;
+        }
+    }
+    cout << ans << endl;
     return 0;
 }

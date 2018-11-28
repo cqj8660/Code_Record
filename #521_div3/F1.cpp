09cbb73e5834cc61c6611129f765a74d401e2bad
Qjchen <cqj8660@163.com>
2018-11-28 13:33:04 +0800

    ..

--- #521_div3/F1.cpp
+++ #521_div3/F1.cpp
@@ -12,17 +12,14 @@ int main()
     cin >> n >> k >> x;
     for(int i = 1; i <= n; i++)
         cin >> a[i];
-    for(int i = 1; i <= n; i++)
-    {
-        dp[i][0] = -1;
-        for(int j = 1; j <= x; j++)
-        {
-            dp[i][j] = -1;
-            for(int t = 1; t <= min(k, i); t++)
-                if(dp[i - t][j - 1] != -1)
-                    dp[i][j] = max(dp[i][j], dp[i - t][j - 1] + a[i]);
-        }
-    }
+    for(int i = 0; i <= n; i++)
+        for(int j = 0; j <= x; j++)
+            dp[i][j] = -1e18;
+    dp[0][0] = 0;
+    for(int i = 1; i <= x; i++)
+        for(int j = 1; j <= n; j++)
+            for(int t = 1; t <= min(k, j); t++)
+                    dp[j][i] = max(dp[j][i], dp[j - t][i - 1] + a[j]);
     ll ans = -1;
     for(int i = n - k + 1; i <= n; i++)
         ans = max(ans, dp[i][x]);

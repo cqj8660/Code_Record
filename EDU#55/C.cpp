b56c5772fe5fb28bd6810aa35fff57bffd112fbc
Qjchen <cqj8660@163.com>
2018-12-07 02:56:34 +0800

    Edu 55 D damn it

--- EDU#55/C.cpp
+++ EDU#55/C.cpp
@@ -5,12 +5,12 @@
 const int maxn = 2e5 + 10;
 using namespace std;
 vector<ll> p[maxn];
-vector<ll> sum[maxn];
-int len;
+ll sum[maxn];
 bool cmp(ll a, ll b)
 {
     return a > b;
 }
+ll ans;
 int main()
 {
     ios::sync_with_stdio(false);
@@ -26,31 +26,19 @@ int main()
     for(int i = 1; i <= m; i++)
     {
         sort(p[i].begin(), p[i].end(), cmp);
-        len = max((int)p[i].size(), len);
-        sum[i].resize(p[i].size());
-    }
-    vector<int> per;
-    for(int i = 1; i <= m; i++)
-    {
-        int le = (int)p[i].size();
-        if(le == 0) continue;
-        else
-            per.push_back(i);
-        sum[i][0] = p[i][0];
-        for(int j = 1; j < le; j++)
-            sum[i][j] = sum[i][j - 1] + p[i][j];
-    }
-    ll ans = 0;
-    for(int i = 0; i < len; i++)
-    {
         ll temp = 0;
-        for(auto j: per)
+        int len = (int)p[i].size();
+        for(int j = 0; j < len; j++)
         {
-            if(p[j].size() > i)
-                if(sum[j][i] > 0)
-                    temp += sum[j][i];
+            temp += p[i][j];
+            if(temp > 0)
+            {
+                sum[j] += temp;
+                ans = max(ans, sum[j]);
+            }
+            else
+                break;
         }
-        ans = max(ans, temp);
     }
     cout << ans << endl;
     return 0;
---
+++ EDU#55/D.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+#define fi first
+#define pii pair<int, int>
+#define se second
+#define ll long long
+const int maxn = 2e3 + 10;
+using namespace std;
+pair<int, int> a[maxn];
+vector<int> g[maxn];
+vector<int> toru;
+int len;
+bool cmp(pii a, pii b)
+{
+    return a.se > b.se;
+}
+int get()
+{
+    static int index = 0;
+    return toru[index++];
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int n, sum = 0;
+    cin >> n;
+    for(int i = 1; i <= n; i++)
+    {
+        cin >> a[i].se;
+        a[i].fi = i;
+        sum += a[i].se;
+    }
+    if(sum / 2 < n - 1)
+    {
+        cout << "NO" << endl;
+        return 0;
+    }
+    sort(a + 1, a + n + 1, cmp);
+    int head = a[1].fi, tail = a[1].fi, i;
+    for(i = 1; i < n - 1; i++)
+    {
+        if(a[i + 1].se < 2)//大于2的成链
+            break;
+        g[a[i].fi].push_back(a[i + 1].fi);
+        len++;
+        tail = a[i + 1].fi;
+        a[i].se--; a[i + 1].se--;
+    }
+    for(int j = 1; j <= i; j++)
+    {
+        for(int k = 0; k < a[j].se; k++)
+            toru.push_back(a[j].fi);
+        if(j == 1)
+            toru.pop_back();
+    }
+    for(int j = n; j > i; j--)
+    {
+        if(j == n)
+        {
+            g[head].push_back(a[j].fi);
+            len++;
+        }
+        else if(j == n -1)
+        {
+            g[tail].push_back(a[j].fi);
+            len++;
+        }
+        else
+            g[get()].push_back(a[j].fi);
+    }
+    cout << "YES" << ' ' << len << endl << n - 1 << endl;
+    for(int i = 1; i <= n; i++)
+        for(auto idx: g[i])
+            cout << i << ' ' << idx << endl;
+    return 0;
+}

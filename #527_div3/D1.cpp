a69f8c83ee6d06869ab9a244ba4ff8a81bec0226
Qjchen <cqj8660@163.com>
2018-12-21 20:07:13 +0800

    527 D

--- #527_div3/D1.cpp
+++ #527_div3/D1.cpp
@@ -2,35 +2,26 @@
 #define ll long long
 #define pii pair<int, int>
 using namespace std;
-const int maxn = 2e5 + 10;
+const int maxn = 3e5 + 10;
 int a[maxn];
 
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(0);
-    int n, pre = -1, maxh = -1;
+    int n;
     cin >> n;
     for(int i = 0; i < n; i++)
-    {
         cin >> a[i];
-        maxh = max(maxh, a[i]);
-    }
     vector<int> sta;
     for(int i = 0; i < n; i++)
     {
-        if(sta.size() && sta.back() == a[i] && a[i] >= pre)
-        {
+        if(sta.size() && ((sta.back() - a[i]) % 2 == 0))
             sta.pop_back();
-            pre = a[i];
-        }
         else
-        {
             sta.push_back(a[i]);
-            pre = -1;
-        }
     }
-    if(sta.empty() ||(sta.size() == 1 && sta.back() == maxh))
+    if(sta.size() <= 1)
         cout << "YES" << endl;
     else
         cout << "NO" << endl;

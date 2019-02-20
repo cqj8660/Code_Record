e8aa091d35593ab42a2101acdcdf2bfddbf1ef09
Qjchen <cqj8660@163.com>
2019-02-20 22:05:29 +0800

    ..

--- Codeforces/#540_div3/C.cpp
+++ Codeforces/#540_div3/C.cpp
@@ -3,39 +3,30 @@
 #define pii pair<int, int>
 using namespace std;
 const int maxn = 2e3 + 10;
-int a[maxn];
 int tim[maxn];
-int cmp(int a, int b)
-{
-    return a < b;
-}
 int ans[30][30];
 set<int> q;
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(0);
-    int n; cin >> n;
+    int n;
+    cin >> n;
     for(int i = 0; i < n * n; i++)
     {
-        cin >> a[i];
-        q.insert(a[i]);
-        tim[a[i]]++;
+        int t;
+        cin >> t;
+        q.insert(t);
+        tim[t]++;
     }
-    bool f = 1;
     if(n % 2 == 0)
     {
         for(auto idx: q)
             if(tim[idx] % 4)
             {
-                f = 0;
-                break;
+                cout << "NO" << endl;
+                return 0;
             }
-        if(f == 0)
-        {
-            cout << "NO" << endl;
-            return 0;
-        }
         vector<int> p;
         for(auto idx: q)
         {
@@ -44,12 +35,8 @@ int main()
         }
         int idx = 0;
         for(int i = 0; i < n / 2; i++)
-        {
             for(int j = 0; j < n / 2; j++)
-            {
                 ans[i][j] = ans[i][n - 1 - j] = ans[n - 1 - i][j] = ans[n - 1 - i][n - 1 - j] = p[idx++];
-            }
-        }
         cout << "YES" << endl;
         for(int i = 0; i < n; i++, cout << endl)
             for(int j = 0; j < n; j++)
@@ -57,16 +44,12 @@ int main()
     }
     else
     {
-        int odd = 0, tt = 0;
+        int odd = 0;
         vector<int> two;
         for(auto idx: q)
-        {
             if(tim[idx] % 2)
                 odd++;
-            else if(tim[idx] % 4 == 2)
-                tt++;
-        }
-        if(tt > n - 1 || odd > 1)
+        if(odd > 1)
         {
             cout << "NO" << endl;
             return 0;
@@ -84,17 +67,12 @@ int main()
                 tim[idx] -= 2;
             }
         }
-        if(two.size() > n - 1)
+        if(two.size() > n - 1 || (n - 1 - two.size()) % 2)//如果是2的倍数超过n-1或者还差奇数个
         {
             cout << "NO" << endl;
             return 0;
         }
-        if((n - 1 - two.size()) % 2)
-        {
-            cout << "NO" << endl;
-            return 0;
-        }
-        if(two.size() < n - 1)
+        if(two.size() < n - 1)//用4的整数倍的去补
             for(auto idx: q)
             {
                 if(tim[idx] > 0 && tim[idx] % 4 == 0)
@@ -118,12 +96,8 @@ int main()
         }
         int idx = 0;
         for(int i = 0; i < n / 2; i++)
-        {
             for(int j = 0; j < n / 2; j++)
-            {
                 ans[i][j] = ans[i][n - 1 - j] = ans[n - 1 - i][j] = ans[n - 1 - i][n - 1 - j] = p[idx++];
-            }
-        }
         idx = 0;
         for(int i = 0; i < n / 2; i++)
         {
@@ -134,8 +108,6 @@ int main()
         for(int i = 0; i < n; i++, cout << endl)
             for(int j = 0; j < n; j++)
                 cout << ans[i][j] << ' ';
-        
     }
-    
     return 0;
 }

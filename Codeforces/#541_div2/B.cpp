f1d58394f1d7f24ee8f0ca41f55d69b7b69952ce
Qjchen <cqj8660@163.com>
2019-02-24 20:26:37 +0800

    ..

--- Codeforces/#541_div2/B.cpp
+++ Codeforces/#541_div2/B.cpp
@@ -14,11 +14,9 @@ int main()
     int n;
     cin >> n;
     for(int i = 0; i < n; i++)
-    {
         cin >> a[i].l >> a[i].r;
-    }
-    ll mmin = min(a[0].l, a[0].r), mmax = max(a[0].l, a[0].r);
-    ll cnt = min(a[0].l, a[0].r) + 1;
+    int mmin = min(a[0].l, a[0].r), mmax = max(a[0].l, a[0].r);
+    int cnt = mmin + 1;
     int p;
     if(a[0].l < a[0].r)
         p = 0;
@@ -28,47 +26,34 @@ int main()
     {
         if(p == 0)
         {
-            if(a[i].l < mmax)
-            {
-                mmax = max(a[i].l, a[i].r);
-                mmin = min(a[i].l, a[i].r);
-                continue;
-            }
-            else
+            if(a[i].l >= mmax)
             {
                 cnt += min(a[i].l, a[i].r) - mmax + 1;;
                 if(a[i - 1].l == a[i - 1].r)
                     cnt--;
-                mmax = max(a[i].l, a[i].r);
-                mmin = min(a[i].l, a[i].r);
                 if(a[i].l < a[i].r)
                     p = 0;
                 else
                     p = 1;
             }
+            mmax = max(a[i].l, a[i].r);
+            mmin = min(a[i].l, a[i].r);
         }
         else
         {
-            if(a[i].r < mmax)
-            {
-                mmax = max(a[i].l, a[i].r);
-                mmin = min(a[i].l, a[i].r);
-                continue;
-            }
-            else
+            if(a[i].r >= mmax)
             {
                 cnt += min(a[i].l, a[i].r) - mmax + 1;
                 if(a[i - 1].l == a[i - 1].r)
                     cnt--;
-                mmax = max(a[i].l, a[i].r);
-                mmin = min(a[i].l, a[i].r);
                 if(a[i].l < a[i].r)
                     p = 0;
                 else
                     p = 1;
             }
+            mmax = max(a[i].l, a[i].r);
+            mmin = min(a[i].l, a[i].r);
         }
-        
     }
     cout << cnt << endl;
     return 0;

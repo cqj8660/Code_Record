bf6f4732ac5beecaf86900c24fb7c9e6bf5d7470
Qjchen <cqj8660@163.com>
2019-03-31 00:16:17 +0800

    P1541

--- Others/random-list/P1541.cpp
+++ Others/random-list/P1541.cpp
@@ -1,21 +1,43 @@
 #include <bits/stdc++.h>
-typedef long long ll
+typedef long long ll;
 using namespace std;
 const int maxn = 1000 + 10;
 int a[maxn], b[maxn];
-ll dp[150][150];
+int dp[40][40][40][40][5];
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(0);
-//    freopen("/Users/vector/Downloads/testdata.in", "r", stdin);
     int n, m;
     cin >> n >> m;
-    for(int i = 0; i < n; i++)
+    for(int i = 1; i <= n; i++)
         cin >> a[i];
     for(int i = 0; i < m; i++)
-        cin >> b[i];
-
+    {
+        int t;
+        cin >> t;
+        b[t]++;
+    }
+    memset(dp, -0x7f, sizeof(dp));
+    dp[0][0][0][0][1] = a[1];
+    for(int i = 2; i <= n; i++)
+    {
+        for(int c1 = 0; c1 <= b[1]; c1++)
+            for(int c2 = 0; c2 <= b[2]; c2++)
+                for(int c3 = 0; c3 <= b[3]; c3++)
+                    for(int c4 = 0; c4 <= b[4]; c4++)
+                    {
+                        if(i > 4 && c4 >= 1)
+                            dp[c1][c2][c3][c4][i % 5] = max(dp[c1][c2][c3][c4][i % 5], dp[c1][c2][c3][c4 - 1][(i - 4) % 5] + a[i]);
+                        if(i > 3 && c3 >= 1)
+                            dp[c1][c2][c3][c4][i % 5] = max(dp[c1][c2][c3][c4][i % 5], dp[c1][c2][c3 - 1][c4][(i - 3) % 5] + a[i]);
+                        if(i > 2 && c2 >= 1)
+                            dp[c1][c2][c3][c4][i % 5] = max(dp[c1][c2][c3][c4][i % 5], dp[c1][c2 - 1][c3][c4][(i - 2) % 5] + a[i]);
+                        if(c1 >= 1)
+                            dp[c1][c2][c3][c4][i % 5] = max(dp[c1][c2][c3][c4][i % 5], dp[c1 - 1][c2][c3][c4][(i - 1) % 5] + a[i]);
+                    }
+    }
+    cout << dp[b[1]][b[2]][b[3]][b[4]][n % 5] << endl;
     return 0;
 }
 

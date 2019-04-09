ad48a1b38cf94471374c95abfde7152c827d906e
Qjchen <cqj8660@163.com>
2019-04-09 23:20:32 +0800

    ..

--- Vjudge/theme7-segment-tree/B_Segment.cpp
+++ Vjudge/theme7-segment-tree/B_Segment.cpp
@@ -4,11 +4,11 @@ const int maxm = 5e3 + 10;
 const int maxn = 2e5 + 10;
 int a[maxn];
 int n, dat[2 * maxn - 1];
-void init(int n_)
+void init(int N)
 {
     n = 1;
-    while(n < n_)
-        n *= 2;
+    while(n < N)
+        n <<= 1;
     for(int i = 0; i < 2 * n - 1; i++)
         dat[i] = INT_MIN;
 }
@@ -22,47 +22,38 @@ void update(int k, int a)
         dat[k] = max(dat[k * 2 + 1], dat[k * 2 + 2]);
     }
 }
-int query(int a, int b, int k, int l, int r)
+int query(int a, int b, int l, int r, int k)
 {
-    if(r <= a || b <= l)
+    if(a >= r || b <= l)
         return INT_MIN;
-    if(a <= l && r <= b)
+    if(a <= l && b >= r)
         return dat[k];
-    else
-    {
-        int vl = query(a, b, k * 2 + 1, l, (l + r) / 2);
-        int rl = query(a, b, k * 2 + 2, (l + r) / 2, r);
-        return max(vl, rl);
-    }
+    return max(query(a, b, l, (l + r) / 2, k * 2 + 1), query(a, b, (l + r) / 2, r, k * 2 + 2));
 }
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(0);
-    int n_, m, a, b;
+    int N, m;
     string c;
-    while(cin >> n_ >> m)
+    while(cin >> N >> m)
     {
-        init(n_);
-        for(int i = 0; i < n_; i++)
+        init(N);
+        for(int i = 0; i < N; i++)
         {
-            int t;
-            cin >> t;
+            int t; cin >> t;
             update(i, t);
         }
         while(m--)
         {
-            cin >> c >> a >> b;
-            if(c[0] == 'Q')
-            {
-                cout << query(a - 1, b, 0, 0, n) << endl;
-            }
+            string q; int l, r;
+            cin >> q >> l >> r;
+            if(q[0] == 'Q')
+                cout << query(l - 1, r, 0, n, 0) << endl;
             else
-            {
-                update(a - 1, b);
-            }
+                update(l - 1, r);
         }
+        
     }
-
     return 0;
 }

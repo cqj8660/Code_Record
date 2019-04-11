dcf254c41b3c4cecf442fbc8406a202e0ccb97db
Qjchen <cqj8660@163.com>
2019-04-11 19:03:11 +0800

    modit

--- Others/Algorithm-Course/H.cpp
+++ Others/Algorithm-Course/H.cpp
@@ -5,10 +5,9 @@
 #define ll long long
 using namespace std;
 const int maxn = 2e4 + 10;
-ll a[maxn];
+ll a[maxn], pre[maxn];
 ll dp[maxn]; //dp[i]表示以i结尾的该行最后一个单词的最小整齐度
-ll res[maxn];
-ll f(ll q)
+inline ll f(ll q)
 {
     return q * q * q;
 }
@@ -18,40 +17,35 @@ int main()
     while(cin >> n >> m)
     {
         memset(dp, 0, sizeof(dp));
-        memset(res, 0, sizeof(dp));
+        memset(pre, 0, sizeof(dp));
         for(int i = 1; i <= n; i++)
         {
             cin >> a[i];
-            res[i] = res[i - 1] + a[i];
+            pre[i] = pre[i - 1] + a[i];
         }
-        if(res[n] + n - 1 <= m)
+        //M为每行容纳的字符数
+        dp[0] = 0;
+        for(int i = 1; i <= n; i++)
         {
-            cout << 0 << endl;
-            continue;
-        }
-        for(ll i = 1; i <= n; i++)
-        {
-            ll val = f(m - a[i]);
-            dp[i] = dp[i - 1] + val;
-            for(ll j = i - 1; j >= 1; j--)
+            dp[i] = dp[i - 1] + f(m - a[i]);
+            for(int j = i - 1; j >= 1; j--)
             {
-                if(res[i] - res[j - 1] + i - j > m)
+                if(pre[i] - pre[j - 1] + i - j > m)
                     break;
-                dp[i] = min(dp[i], dp[j - 1] + f(m - (res[i] - res[j - 1] + i - j)));
+                dp[i] = min(dp[i], dp[j - 1] + f(m - pre[i] + pre[j - 1] - i + j));
             }
         }
         //        for(int i = 1; i <= n; i++)
         //            cout << dp[i] << endl;
-        ll ans = 1LL << 61;
-        for(int i = n; i >= 1; i--)
+        ll ans = 1LL << 60;
+        for(int i = n; i >= 0; i--)//0表示一行就可以容纳所有单词
         {
-            ll temp = res[n] - res[i - 1];
             ans = min(ans, dp[i]);
-            if(temp + n - i > m)
+            if(pre[n] - pre[i - 1] + n - i > m)
                 break;
         }
         cout << ans << endl;
-        
     }
     
 }
+

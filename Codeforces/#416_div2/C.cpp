60cd5ad226d4d72a2d17ae91b4e001925f785c1b
unknown <cqj8660@163.com>
2019-05-06 01:39:01 +0800

    C

---
+++ Codeforces/#416_div2/C.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+const int maxn = 1e4 + 10;
+int a[maxn];
+int beg[maxn], ter[maxn];
+int dp[maxn];//dp[i]表示前i个数字最大的值
+bool vis[maxn];
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int n;
+    cin >> n;
+    for(int i = 1; i <= n; i++)
+        {
+            cin >> a[i];
+            if(beg[a[i]] == 0)
+                beg[a[i]] = i;
+            ter[a[i]] = i;
+        }
+    for(int i = 1; i <= n; i++){
+        dp[i] = dp[i - 1];
+        memset(vis, 0, sizeof(vis));
+        int res = 0, st = i;
+        for(int j = i; j >= 0; j--){
+            if(vis[a[j]] == 0){
+                if(ter[a[j]] > i) break;
+                vis[a[j]] = 1;
+                st = min(st, beg[a[j]]);
+                res ^= a[j];
+            }
+            if(j <= st)
+                dp[i] = max(dp[i], dp[j - 1] + res);
+        }
+    }
+    cout << dp[n] << endl;
+    return 0;
+}

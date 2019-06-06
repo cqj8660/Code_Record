63bd5b4804c0eabaeed090eb393e5cce3da04daf
cqj86 <cqj8660@163.com>
2019-06-07 00:56:20 +0800

    420 C

--- Codeforces/#420_div2/C.cpp
+++ Codeforces/#420_div2/C.cpp
@@ -1,11 +1,42 @@
 #include <bits/stdc++.h>
 #define ll long long
 using namespace std;
-const int maxn = 1e4 + 10;
+vector<int> sta;
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(0);
-    
+    int n;
+    cin >> n;
+    int exp = 1, cnt = 0;
+    for(int i = 0; i < 2 * n; i++)
+    {
+        string s;
+        int x;
+        cin >> s;
+        if(s[0] == 'a')
+        {
+            cin >> x;
+            sta.push_back(x);
+        }
+        else
+        {
+            if(sta.empty())
+            {
+                exp++;
+                continue;
+            }
+            if(sta.back() == exp++)
+            {
+                sta.pop_back();
+            }
+            else
+            {
+                cnt++;
+                sta.clear();
+            }
+        }
+    }
+    cout << cnt << endl;
     return 0;
 }
